3db020bda3940a0cce8eda05c05018bbdaeaa35e
ronaldo7-tobi <t.szerszen@zset.leszno.pl>
2024-11-26 19:52:28 +0100

    Add files via upload
    Do pracy na jutro

---
+++ quizlet_projekt/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizlet by Tobiasz & Daniel</title>
+
+    <!--Stylesheets -->
+    <link rel="stylesheet" href="styles/basic_body_styles.css">
+    <link rel="stylesheet" href="styles/nav_styles.css">
+</head>
+<body>
+    <nav>
+        <a href="register.php">Zarejstruj się</a>
+        <a href="login.php">Zaloguj się</a>
+    </nav>
+
+    <header>
+        <h1>Witaj na najlepszym serwisie do fiszek!</h1>
+        <h3>Autorzy: Tobiasz Szerszeń i Daniel Teślak</h3>
+    </header>
+
+    <section class="main_section">
+
+    </section>
+</body>
+</html>
