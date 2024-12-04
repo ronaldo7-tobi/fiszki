04d0f306a73eb68cb4118dd5191269a3f92ff0cf
ronaldo7-tobi <t.szerszen@zset.leszno.pl>
2024-12-04 16:30:08 +0100

    Add files via upload

---
+++ quizlet_projekt/pages/logout.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+
+    // Zakończ sesję
+    session_unset();
+    session_destroy();
+
+    // Przekieruj na stronę logowania
+    header("Location: login.php");
+    exit;
+?>
